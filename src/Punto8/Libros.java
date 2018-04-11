ddf37d67a6efc0117e57efdf131b9596dc2463e6
germa <germa@10.7.3.51>
2018-04-11 16:31:43 -0300

    punto 8

---
+++ src/Punto8/Libros.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto8;
+
+/**
+ *
+ * @author germa
+ */
+public class Libros {
+    
+    private int isbn;
+    private String titulo;
+    private String autor;
+    private double precio;
+
+    public Libros() {
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+}
---
+++ src/Punto8/Punto8.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto8;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author germa
+ */
+public class Punto8 {
+    
+    public static void main(String[] args){
+        
+        ArrayList<Libros> librosArray = new ArrayList<Libros>();
+        
+        System.out.println("Ingrese el ISBN del libro: ");
+        Scanner scanner1 = new Scanner(System.in);
+        int isbn = scanner1.nextInt();
+        
+         
+        
+        
+       
+    }
+}
