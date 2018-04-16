18f3b4be9be33ddc92dcb1aab1d9ab2c80b10ca8
Windows 10 <Windows 10@DESKTOP-QIEAUQV>
2018-04-16 14:52:16 -0300

    Punto 11 listo

---
+++ src/tp03/ejercicio11/TestEjer11.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp03.ejercicio11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class TestEjer11 {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.println("Ingrese la marca de la zapatilla: ");
+        String marc = scanner.nextLine();
+}
+}
